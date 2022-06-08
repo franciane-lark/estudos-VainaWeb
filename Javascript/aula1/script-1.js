46beb1c07292a0f753b8e4b7c6efa8130265883b
Franciane Lark <franciane.lark019@gmail.com>
2022-06-08 18:36:22 -0400

    update js

---
+++ Javascript/aula1/script-1.js
@@ -0,0 +1,18 @@
+// Crie uma variável que receba um nome;
+// Crie uma variável que receba um número;
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+// Mostre no console a mensagem "Estou mexendo no console.log";
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano 
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+let nome = "Eliana";
+let numero = 1;
+let anoAtual = 2022 - numero;
+
+console.log(anoAtual);
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = true;
+
+console.log(typeof quartaFeira );
---
+++ Javascript/aula1/script.js
@@ -0,0 +1,14 @@
+//Declaração de varivável: var, let, const
+
+var nome = "Welder"
+var idade = 32;
+
+console.log(nome, idade);
+
+const cpf = 222;//const só pode receber um único valor. Não muda.
+console.log(cpf);
+
+let nota1 = 8;
+let nota2 = 10;
+let media = (nota1 + nota2)/2;
+console.log("A sua nota é ${media}");
