091f6eb70a349323532c14b0d79cf4bc9ac25633
Franciane Lark <franciane.lark019@gmail.com>
2022-06-10 18:44:07 -0400

    update js aula2

---
+++ Javascript/aula2/script.js
@@ -0,0 +1,7 @@
+let mesNiver1 = "janeiro";
+let mesNiver2 = "dezembro";
+if (mesNiver1 === "janeiro" || mesNiver2 === "dezembro"){
+    console.log("mes Janeiro ou dezembro válido!!");
+}else {
+    console.log("mes inválido");
+}
