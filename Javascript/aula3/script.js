fbd3c2aff43635b35909d8f2442db28c7ed64f76
Franciane Lark <franciane.lark019@gmail.com>
2022-06-13 10:21:19 -0400

    update aula3 js

---
+++ Javascript/aula3/script.js
@@ -0,0 +1,58 @@
+// // Operadores de Atribuição
+
+// let conta = 11;
+// // soma = soma + 4;
+// conta += 4;
+// console.log("Soma:", conta);
+
+// conta -= 5;
+// console.log("Subtração:", conta);
+
+// conta *= 2;
+// console.log("Multiplicação:", conta);
+
+// conta /= 4;
+// console.log("Divisão:", conta);
+
+// conta %= 2;
+// console.log("Resto da divisão:", conta);
+
+// conta **= 4;
+// console.log("Expoente:", conta);
+
+// //====================================================================
+
+// // While Loop
+// let numero = 0;
+
+// while( numero <= 100){
+//     console.log("Repetição com o While:", numero);
+//     numero++ ;
+// }
+
+// //====================================================================
+
+// // FOR LOOP
+
+// // Início (criando a variavel), condição, incremento
+// for(let number = 10; number >= 0; number--){
+//     console.log("Repetição com o For Loop:", number);
+// }
+
+
+// for(let teste = 0; teste <= 100; teste++){
+//     if(teste %3 === 0 ){
+//         console.log(teste);
+//     }
+// }
+
+//Criar um loop que conte todos os números
+// ímpares de 0 até 100 usando for e while
+
+for(let numero = 0; numero <= 10; numero++){
+   console.log("Número incrementado: ", numero);
+    // if(numero % 2 === 0){
+    //     console.log(numero);
+    // }
+
+}
